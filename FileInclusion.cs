1a88a83598fb8e5566da29c8419c8ab0acd81a32
AnLoffredo <AnLoffredo@massageenvy.com>
2021-08-31 12:03:53 -0700

    Autocommit
    Autocommit

---
+++ FileInclusion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Penguin.IO
+{
+    public class FileInclusion
+    {
+        public string Path { get; set; }
+        public bool Recursive { get; set; } = true;
+        public bool Exclude { get; set; } = false;
+
+        public FileInclusion(string path, bool include = true, bool recursive = true)
+        {
+            this.Path = path;
+            this.Recursive = recursive;
+            this.Exclude = !include;
+        }
+
+        public FileInclusion()
+        {
+
+        }
+
+        public SearchOption SearchOption => Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+        public static implicit operator List<FileInclusion>(FileInclusion f) => new List<FileInclusion>() { f };
+        public static implicit operator FileInclusion(string path) => new FileInclusion(path);
+    }
+}
---
+++ FileList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Penguin.IO
+{
+    public class FileList : IEnumerable<string>
+    {
+        HashSet<string> Results = new HashSet<string>();
+        
+        private string Root { get; set; }
+        
+        public FileList(string root = null)
+        {
+            this.Root = root ?? Directory.GetCurrentDirectory();
+        }
+
+        public void AddRule(FileInclusion inclusion)
+        {
+            if (inclusion is null)
+            {
+                throw new ArgumentNullException(nameof(inclusion));
+            }
+
+            string testPath = Path.Combine(this.Root, inclusion.Path);
+
+            List<string> files;
+
+            if (Directory.Exists(testPath))
+            {
+                files = Directory.EnumerateFiles(testPath, "*", inclusion.SearchOption).ToList();
+            }
+            else if (File.Exists(testPath))
+            {
+                files = new List<string> { testPath };
+            }
+            else
+            {
+                files = Directory.EnumerateFiles(this.Root, inclusion.Path, inclusion.SearchOption).ToList();
+            }
+
+            foreach (string file in files)
+            {
+                _ = inclusion.Exclude ? Results.Remove(file) : Results.Add(file);
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator() => this.Results.ToList().GetEnumerator();
+        
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
